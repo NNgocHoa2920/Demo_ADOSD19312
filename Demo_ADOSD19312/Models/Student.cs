888fd70b2bef5194383f8c26f5a1a775d3cb5c3e
Hoa Nguyễn Ngọc <nguyenngochoa292000@gmail.com>
2024-10-08 10:26:26 +0700

    UPDATE

--- Demo_ADOSD19312/Models/Student.cs
+++ Demo_ADOSD19312/Models/Student.cs
@@ -1,7 +1,9 @@
 ﻿namespace Demo_ADOSD19312.Models
 {
     public class Student
-    {
+    {/// <summary>
+    /// HELLO CÁC BẠN
+    /// </summary>
         public int Id { get; set; }
         public string Name { get; set; }
         public string Gender { get; set; }
